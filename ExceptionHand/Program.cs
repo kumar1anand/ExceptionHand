3a5f4ab3ad0bed42d2a0a1c6d8e37f11aa7b144f
Anand.Kumar <Anand.Kumar@INLPF3KSCQZ>
2022-09-26 15:43:17 +0530

    Add project files.

---
+++ ExceptionHand/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExceptionHand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+           FileStream fs= new FileStream("C:\\Users\\anand.kumar\\OneDrive - Entain Group\\Documents\\DA_Assignment\\test.txt", FileMode.OpenOrCreate);
+            //fs.WriteByte(68);
+
+            StreamWriter sw= new StreamWriter(fs);
+            sw.WriteLine("Hello shivam how are u?");
+            sw.Close();
+            /*sr.WriteLine();
+            while ()
+            {
+
+            }*/
+            fs.Close();
+        }
+    }
+}
